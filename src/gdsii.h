5c019b3f0929a584bc158aab123437b701d8737e
dteal <dteal@dteal.org>
2020-04-11 22:11:44 -0700

    fixes GDSII integer parsing

--- src/gdsii.h
+++ src/gdsii.h
@@ -3,6 +3,7 @@
 
 // GDSII file format parser
 // very minimal; extracts layer geometry only
+// written partly in ANSI C style for future possibilities
 
 #include <assert.h>
 #include <stdio.h>
@@ -11,71 +12,67 @@
 #include <vector>
 #include <string>
 
-// just in case
-typedef uint8_t UINT8;
-typedef int16_t INT16;
-typedef int32_t INT32;
 typedef float REAL32;
 typedef double REAL64;
 
 // possibly non-exaustive list of record types
-const UINT8 RECORD_TYPE_HEADER = 0x00;
-const UINT8 RECORD_TYPE_BGNLIB = 0x01;
-const UINT8 RECORD_TYPE_LIBNAME = 0x02;
-const UINT8 RECORD_TYPE_UNITS = 0x03;
-const UINT8 RECORD_TYPE_ENDLIB = 0x04;
-const UINT8 RECORD_TYPE_BGNSTR = 0x05;
-const UINT8 RECORD_TYPE_STRNAME = 0x06;
-const UINT8 RECORD_TYPE_ENDSTR = 0x07;
-const UINT8 RECORD_TYPE_BOUNDARY = 0x08;
-const UINT8 RECORD_TYPE_PATH = 0x09;
-const UINT8 RECORD_TYPE_SREF = 0x0a;
-const UINT8 RECORD_TYPE_AREF = 0x0b;
-const UINT8 RECORD_TYPE_TEXT = 0x0c;
-const UINT8 RECORD_TYPE_LAYER = 0x0d;
-const UINT8 RECORD_TYPE_DATATYPE = 0x0e;
-const UINT8 RECORD_TYPE_WIDTH = 0x0f;
-const UINT8 RECORD_TYPE_XY = 0x10;
-const UINT8 RECORD_TYPE_ENDEL = 0x11;
-const UINT8 RECORD_TYPE_SNAME = 0x12;
-const UINT8 RECORD_TYPE_COLROW = 0x13;
-const UINT8 RECORD_TYPE_TEXTNODE = 0x14;
-const UINT8 RECORD_TYPE_NODE = 0x15;
-const UINT8 RECORD_TYPE_TEXTTYPE = 0x16;
-const UINT8 RECORD_TYPE_PRESENTATION = 0x17;
-const UINT8 RECORD_TYPE_STRING = 0x19;
-const UINT8 RECORD_TYPE_STRANS = 0x1a;
-const UINT8 RECORD_TYPE_MAG = 0x1b;
-const UINT8 RECORD_TYPE_ANGLE = 0x1c;
-const UINT8 RECORD_TYPE_REFLIBS = 0x1f;
-const UINT8 RECORD_TYPE_FONTS = 0x20;
-const UINT8 RECORD_TYPE_PATHTYPE = 0x21;
-const UINT8 RECORD_TYPE_GENERATIONS = 0x22;
-const UINT8 RECORD_TYPE_ATTRTABLE = 0x23;
-const UINT8 RECORD_TYPE_ELFLAGS = 0x26;
-const UINT8 RECORD_TYPE_NODETYPE = 0x2a;
-const UINT8 RECORD_TYPE_PROPATTR = 0x2b;
-const UINT8 RECORD_TYPE_PROPVALUE = 0x2c;
-const UINT8 RECORD_TYPE_BOX = 0x2d;
-const UINT8 RECORD_TYPE_BOXTYPE = 0x2e;
-const UINT8 RECORD_TYPE_PLEX = 0x2f;
-const UINT8 RECORD_TYPE_TAPENUM = 0x32;
-const UINT8 RECORD_TYPE_TAPECODE = 0x33;
-const UINT8 RECORD_TYPE_FORMAT = 0x36;
-const UINT8 RECORD_TYPE_MASK = 0x37;
-const UINT8 RECORD_TYPE_ENDMASKS = 0x38;
+const uint8_t RECORD_TYPE_HEADER = 0x00;
+const uint8_t RECORD_TYPE_BGNLIB = 0x01;
+const uint8_t RECORD_TYPE_LIBNAME = 0x02;
+const uint8_t RECORD_TYPE_UNITS = 0x03;
+const uint8_t RECORD_TYPE_ENDLIB = 0x04;
+const uint8_t RECORD_TYPE_BGNSTR = 0x05;
+const uint8_t RECORD_TYPE_STRNAME = 0x06;
+const uint8_t RECORD_TYPE_ENDSTR = 0x07;
+const uint8_t RECORD_TYPE_BOUNDARY = 0x08;
+const uint8_t RECORD_TYPE_PATH = 0x09;
+const uint8_t RECORD_TYPE_SREF = 0x0a;
+const uint8_t RECORD_TYPE_AREF = 0x0b;
+const uint8_t RECORD_TYPE_TEXT = 0x0c;
+const uint8_t RECORD_TYPE_LAYER = 0x0d;
+const uint8_t RECORD_TYPE_DATATYPE = 0x0e;
+const uint8_t RECORD_TYPE_WIDTH = 0x0f;
+const uint8_t RECORD_TYPE_XY = 0x10;
+const uint8_t RECORD_TYPE_ENDEL = 0x11;
+const uint8_t RECORD_TYPE_SNAME = 0x12;
+const uint8_t RECORD_TYPE_COLROW = 0x13;
+const uint8_t RECORD_TYPE_TEXTNODE = 0x14;
+const uint8_t RECORD_TYPE_NODE = 0x15;
+const uint8_t RECORD_TYPE_TEXTTYPE = 0x16;
+const uint8_t RECORD_TYPE_PRESENTATION = 0x17;
+const uint8_t RECORD_TYPE_STRING = 0x19;
+const uint8_t RECORD_TYPE_STRANS = 0x1a;
+const uint8_t RECORD_TYPE_MAG = 0x1b;
+const uint8_t RECORD_TYPE_ANGLE = 0x1c;
+const uint8_t RECORD_TYPE_REFLIBS = 0x1f;
+const uint8_t RECORD_TYPE_FONTS = 0x20;
+const uint8_t RECORD_TYPE_PATHTYPE = 0x21;
+const uint8_t RECORD_TYPE_GENERATIONS = 0x22;
+const uint8_t RECORD_TYPE_ATTRTABLE = 0x23;
+const uint8_t RECORD_TYPE_ELFLAGS = 0x26;
+const uint8_t RECORD_TYPE_NODETYPE = 0x2a;
+const uint8_t RECORD_TYPE_PROPATTR = 0x2b;
+const uint8_t RECORD_TYPE_PROPVALUE = 0x2c;
+const uint8_t RECORD_TYPE_BOX = 0x2d;
+const uint8_t RECORD_TYPE_BOXTYPE = 0x2e;
+const uint8_t RECORD_TYPE_PLEX = 0x2f;
+const uint8_t RECORD_TYPE_TAPENUM = 0x32;
+const uint8_t RECORD_TYPE_TAPECODE = 0x33;
+const uint8_t RECORD_TYPE_FORMAT = 0x36;
+const uint8_t RECORD_TYPE_MASK = 0x37;
+const uint8_t RECORD_TYPE_ENDMASKS = 0x38;
 
-const UINT8 DATA_TYPE_NODATA = 0x00;
-const UINT8 DATA_TYPE_BITARRAY = 0x01;
-const UINT8 DATA_TYPE_INT16 = 0x02;
-const UINT8 DATA_TYPE_INT32 = 0x03;
-const UINT8 DATA_TYPE_REAL32 = 0x04;
-const UINT8 DATA_TYPE_REAL64 = 0x05;
-const UINT8 DATA_TYPE_ASCII = 0x06;
+const uint8_t DATA_TYPE_NODATA = 0x00;
+const uint8_t DATA_TYPE_BITARRAY = 0x01;
+const uint8_t DATA_TYPE_INT16 = 0x02;
+const uint8_t DATA_TYPE_INT32 = 0x03;
+const uint8_t DATA_TYPE_REAL32 = 0x04;
+const uint8_t DATA_TYPE_REAL64 = 0x05;
+const uint8_t DATA_TYPE_ASCII = 0x06;
 
-const UINT8 PATH_TYPE_FLUSH = 0x00;
-const UINT8 PATH_TYPE_ROUND = 0x01;
-const UINT8 PATH_TYPE_SQUARE = 0x02;
+const uint8_t PATH_TYPE_FLUSH = 0x00;
+const uint8_t PATH_TYPE_ROUND = 0x01;
+const uint8_t PATH_TYPE_SQUARE = 0x02;
 
 enum ELEMENT_TYPE{ boundary, path, sref, aref, box };
 
@@ -83,9 +80,9 @@ struct XY{ REAL32 x; REAL32 y; XY* next; };
 
 struct GDSII_ELEMENT{
     ELEMENT_TYPE type;
-    INT16 layer;
-    INT16 width;
-    UINT8 path_type;   
+    int16_t layer;
+    int16_t width;
+    uint8_t path_type;   
     GDSII_ELEMENT* selement;
     XY* xy;
     bool use_strans;
@@ -100,7 +97,7 @@ struct GDSII_ELEMENT{
 };
 
 struct GDSII_STRUCTURE{
-    char* sname;
+    char* name;
     GDSII_ELEMENT* element;
     GDSII_STRUCTURE* next;
 };
@@ -113,11 +110,40 @@ std::vector<int16_t> parse_int16(uint8_t* data, uint16_t length){
     assert(length%2==0);
     std::vector<int16_t> newdata;
     for(uint i=0; i<length/2; i++){
-        newdata.push_back((data[2*i] << 1) + data[2*i+1]);
+        newdata.push_back((data[2*i+0] << 8) + data[2*i+1]);
     }
     return newdata;
 }
 
+std::vector<int32_t> parse_int32(uint8_t* data, uint16_t length){
+    assert(length%4==0);
+    std::vector<int32_t> newdata;
+    for(uint i=0; i<length/4; i++){
+        newdata.push_back((data[4*i+0] << 24) + (data[4*i+1] << 16) + (data[4*i+2] << 8) + data[4*i+3]);
+    }
+    return newdata;
+}
+
+/*
+ * NOT IEEE754!
+std::vector<REAL64> parse_real64(uint8_t* data, uint16_t length){
+    assert(length%8==0);
+    std::vector<REAL64> newdata;
+    for(uint i=0; i<length/8; i++){
+        int sign = 1;
+        if(data[0]&0x80) sign=-1;
+        // TODO: finish this
+        //newdata.push_back((data[4*i] << 3) + (data[4*i+1] << 2) + (data[4*i+2] << 1) + data[4*i+3]);
+    }
+    return newdata;
+}
+*/
+
+std::string parse_string(uint8_t* data, uint16_t){
+    std::string result = "";
+    result += (char*)data;
+    return result;
+}
 
 void gdsii_free (GDSII* gdsii){
     GDSII_STRUCTURE* structure = (*gdsii).structure;
@@ -159,7 +185,7 @@ GDSII* gdsii_read(const char* filepath){
         uint16_t length = (buffer[0] << 1) + buffer[1] - 4; // length of data, not entire record
         uint8_t record_type = buffer[2];
         uint8_t data_type = buffer[3];
-        std::cout << "RECORD: length: " << (int)length << " type: " << (int)record_type << " data: " << (int)data_type << std::endl;
+        std::cout << "RECORD: length: " << (int)(length+4) << " type: " << (int)record_type << " data: " << (int)data_type << std::endl;
 
         // read record data
         uint8_t* data = nullptr;
@@ -173,7 +199,7 @@ GDSII* gdsii_read(const char* filepath){
                 printf("units\n");
                 break;
             case RECORD_TYPE_BGNSTR:
-                printf("begin structure\n");
+                printf("structure begin\n");
                 if(first_structure){ first_structure = false; }else{
                     (*structure).next = (GDSII_STRUCTURE*)malloc(sizeof(GDSII_STRUCTURE));
                     structure = structure->next;
@@ -181,52 +207,65 @@ GDSII* gdsii_read(const char* filepath){
                     (*structure).element = element; }
                 break;
             case RECORD_TYPE_ENDSTR:
-                printf("end structure\n");
+                printf("structure end\n");
                 structure->next = NULL;
                 break;
             case RECORD_TYPE_STRNAME:
-                printf("structure name\n");
+                printf("structure name: ");
+                //structure->name = (char*)parse_string(data, length).c_str();
+                //std::cout << structure->name << std::endl;
                 break;
             case RECORD_TYPE_ENDEL:
-                printf("endel\n");
+                printf("\tendel\n");
                 element->next = NULL;
                 break;
             case RECORD_TYPE_BOUNDARY:
-                printf("boundary\n");
+                printf("\tboundary\n");
                 if(first_element){ first_element = false; }else{
-                    element->next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
-                    element = element->next; }
+                    (*element).next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
+                    element = (*element).next; }
                 break;
             case RECORD_TYPE_PATH:
-                printf("path\n");
+                printf("\tpath\n");
                 if(first_element){ first_element = false; }else{
-                    element->next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
-                    element = element->next; }
+                    (*element).next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
+                    element = (*element).next; }
                 break;
             case RECORD_TYPE_SREF:
-                printf("sref\n");
+                printf("\tsref\n");
                 if(first_element){ first_element = false; }else{
-                    element->next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
-                    element = element->next; }
+                    (*element).next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
+                    element = (*element).next; }
                 break;
             case RECORD_TYPE_AREF:
-                printf("aref\n");
+                printf("\taref\n");
                 if(first_element){ first_element = false; }else{
-                    element->next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
-                    element = element->next; }
+                    (*element).next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
+                    element = (*element).next; }
                 break;
             case RECORD_TYPE_BOX:
-                printf("box\n");
+                printf("\tbox\n");
                 if(first_element){ first_element = false; }else{
-                    element->next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
-                    element = element->next; }
+                    (*element).next = (GDSII_ELEMENT*)malloc(sizeof(GDSII_ELEMENT));
+                    element = (*element).next; }
                 break;
             case RECORD_TYPE_XY:
-                printf("xy\n");
+                printf("\t\txy\n");
+                if(data_type == DATA_TYPE_INT32){
+                    std::vector<int32_t>points = parse_int32(data, length);
+                    for(unsigned int i=0; i<points.size(); i++){
+                        std::cout << (int)points[i] << " ";
+                }
+                    std::cout << std::endl;
+
+                }
                 break;
             case RECORD_TYPE_LAYER:
-                printf("layer\n");
-                //(*element).layer = 0;
+                printf("\t\tlayer\n");
+                if(data_type == DATA_TYPE_INT16){
+                    std::vector<int16_t>points = parse_int16(data, length);
+                    (*element).layer = points[0];
+                }
                 break;
         }
 
