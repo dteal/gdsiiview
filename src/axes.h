b2b428527cded82b43bc408da0d07d2e67f9df7a
dteal <dteal@dteal.org>
2020-05-13 19:20:08 -0700

    clean up code

--- src/axes.h
+++ src/axes.h
@@ -1,25 +1,23 @@
 #ifndef AXES_H
 #define AXES_H
 
-#include <QOpenGLFunctions>
-#include <QOpenGLVertexArrayObject>
-#include <QOpenGLBuffer>
-#include <QOpenGLShaderProgram>
-#include "glm/glm.hpp"
-#include "glm/gtc/type_ptr.hpp"
+#include <QOpenGLFunctions>             // OpenGL functions
+#include <QOpenGLVertexArrayObject>     // OpenGL vertex array
+#include <QOpenGLBuffer>                // OpenGL vertex buffer
+#include <QOpenGLShaderProgram>         // OpenGL shader
+#include <QSharedPointer>               // for safer pointer handling
+#include "glm/glm.hpp"                  // GLM matrices
+#include "glm/gtc/type_ptr.hpp"         // GLM matrices to OpenGL data
 
 class Axes : protected QOpenGLFunctions {
 public:
 
-    glm::vec3 location = glm::vec3(0.0f, 0.0f, 0.0f);
-    //float radius = 250.0f;
-    float radius = 0.4f;
-    QOpenGLVertexArrayObject* axis_VAO;
-    QOpenGLBuffer* axis_VBO;
-    QOpenGLVertexArrayObject* circle_VAO;
-    QOpenGLBuffer* circle_VBO;
+    QSharedPointer<QOpenGLVertexArrayObject> axis_VAO;
+    QSharedPointer<QOpenGLBuffer> axis_VBO;
+    QSharedPointer<QOpenGLVertexArrayObject> circle_VAO;
+    QSharedPointer<QOpenGLBuffer> circle_VBO;
+    QSharedPointer<QOpenGLShaderProgram> shader;
     unsigned int circle_num_vertices = 100;
-    QOpenGLShaderProgram* shader = nullptr;
 
 // this is messy, but easier than separate files
 const char* vertex_source = "                           \n\
@@ -40,64 +38,66 @@ const char* fragment_source = "                         \n\
 Axes()  {
     initializeOpenGLFunctions();
 
-    // make unit line on x axis
-    float axis_vertices[] = {
+    // make a unit line on the x axis
+    GLfloat axis_vertices[] = {
         0.0f, 0.0f, 0.0f,
         1.0f, 0.0f, 0.0f,
     };
-    axis_VAO = new QOpenGLVertexArrayObject();
-    axis_VBO = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
+    axis_VAO = QSharedPointer<QOpenGLVertexArrayObject>(new QOpenGLVertexArrayObject());
+    axis_VBO = QSharedPointer<QOpenGLBuffer>(new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer));
     axis_VAO->create();
     axis_VAO->bind();
     axis_VBO->create();
-    axis_VBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
     axis_VBO->bind();
-    axis_VBO->allocate(axis_vertices, sizeof(float)*2*3);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    axis_VBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
+    axis_VBO->allocate(axis_vertices, sizeof(GLfloat)*2*3);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
     axis_VAO->release();
 
-    // make unit circle on yz plane
-    float dtheta = 2*M_PI/circle_num_vertices;
-    float* circle_vertices = new float[circle_num_vertices*3];
+    // make a unit circle on the yz plane
+    float circle_dtheta = 2*M_PI/circle_num_vertices;
+    GLfloat* circle_vertices = new GLfloat[circle_num_vertices*3];
     for(unsigned int i=0; i<circle_num_vertices; i++){
         circle_vertices[3*i+0] = 0.0f;
-        circle_vertices[3*i+1] = cos(i*dtheta);
-        circle_vertices[3*i+2] = sin(i*dtheta);
+        circle_vertices[3*i+1] = cos(i*circle_dtheta);
+        circle_vertices[3*i+2] = sin(i*circle_dtheta);
     }
-    circle_VAO = new QOpenGLVertexArrayObject();
-    circle_VBO = new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
+    circle_VAO = QSharedPointer<QOpenGLVertexArrayObject>(new QOpenGLVertexArrayObject());
+    circle_VBO = QSharedPointer<QOpenGLBuffer>(new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer));
     circle_VAO->create();
     circle_VAO->bind();
     circle_VBO->create();
-    circle_VBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
     circle_VBO->bind();
-    circle_VBO->allocate(circle_vertices, sizeof(float)*circle_num_vertices*3);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    circle_VBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
+    circle_VBO->allocate(circle_vertices, sizeof(GLfloat)*circle_num_vertices*3);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
     circle_VAO->release();
     delete[] circle_vertices;
 
     // make shader
-    shader = new QOpenGLShaderProgram();
+    shader = QSharedPointer<QOpenGLShaderProgram>(new QOpenGLShaderProgram());
     shader->addShaderFromSourceCode(QOpenGLShader::Vertex, vertex_source);
     shader->addShaderFromSourceCode(QOpenGLShader::Fragment, fragment_source);
     shader->link();
 }
 
-// free GPU memory
 ~Axes(){
-    delete circle_VBO;
-    delete circle_VAO;
-    delete shader;
+    // assume there is an active OpenGL context
+    axis_VAO->release();
+    axis_VBO->release();
+    circle_VAO->release();
+    circle_VBO->release();
+    shader->release();
 }
 
-// draw axes
 void render(glm::mat4 transform){
     shader->bind();
     glLineWidth(2.0f);
-    transform = glm::translate(transform, location);
-    transform = glm::scale(transform, glm::vec3(radius, radius, radius));
+
+    float circle_radius = 0.4f; // fill 0.4*2 = 80% of canvas height
+    transform = glm::scale(transform, glm::vec3(circle_radius, circle_radius, circle_radius));
     unsigned int transform_id = glGetUniformLocation(shader->programId(), "transform");
 
     // draw x axis and circle
@@ -117,7 +117,7 @@ void render(glm::mat4 transform){
     glDrawArrays(GL_LINE_LOOP, 0, circle_num_vertices);
     circle_VAO->release();
     axis_VAO->bind();
-    glDrawArrays(GL_LINES, 0, circle_num_vertices);
+    glDrawArrays(GL_LINES, 0, 2);
     axis_VAO->release();
 
     // draw z axis and circle
@@ -127,8 +127,10 @@ void render(glm::mat4 transform){
     glDrawArrays(GL_LINE_LOOP, 0, circle_num_vertices);
     circle_VAO->release();
     axis_VAO->bind();
-    glDrawArrays(GL_LINES, 0, circle_num_vertices);
+    glDrawArrays(GL_LINES, 0, 2);
     axis_VAO->release();
+
+    shader->release();
 }
 
 };
